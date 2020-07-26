140e8ee0adaf90dc948f9f927fe4fe7b3c562abd
Arpit Jalan <arpit@techapj.com>
2020-07-26 20:30:57 +0530

    WIP: markdown parser for surveys

--- lib/discourse-surveys/survey.rb
+++ lib/discourse-surveys/survey.rb
@@ -3,7 +3,30 @@
 module DiscourseSurvey
   class Survey
     class << self
-      #
+
+      def extract(raw, topic_id, user_id = nil)
+        cooked = PrettyText.cook(raw, topic_id: topic_id, user_id: user_id)
+
+        Nokogiri::HTML5(cooked).css("div.survey").map do |p|
+          poll = { "options" => [], "name" => DiscoursePoll::DEFAULT_SURVEY_NAME }
+
+          # attributes
+          p.attributes.values.each do |attribute|
+            if attribute.name.start_with?(DATA_PREFIX)
+              poll[attribute.name[DATA_PREFIX.length..-1]] = CGI.escapeHTML(attribute.value || "")
+            end
+          end
+
+          # options
+          p.css("li[#{DATA_PREFIX}option-id]").each do |o|
+            option_id = o.attributes[DATA_PREFIX + "option-id"].value.to_s
+            poll["options"] << { "id" => option_id, "html" => o.inner_html.strip }
+          end
+
+          poll
+        end
+      end
+
     end
   end
 end
--- lib/discourse-surveys/survey_validator.rb
+++ lib/discourse-surveys/survey_validator.rb
@@ -2,6 +2,30 @@
 
 module DiscourseSurvey
   class SurveyValidator
-    #
+
+    MAX_VALUE = 2_147_483_647
+
+    def initialize(post)
+      @post = post
+    end
+
+    def validate_surveys
+      surveys = {}
+
+      Discoursesurvey::Survey::extract(@post.raw, @post.topic_id, @post.user_id).each do |survey|
+        # return false unless valid_arguments?(survey)
+        # return false unless valid_numbers?(survey)
+        # return false unless unique_survey_name?(surveys, survey)
+        # return false unless unique_options?(survey)
+        # return false unless any_blank_options?(survey)
+        # return false unless at_least_one_option?(survey)
+        # return false unless valid_number_of_options?(survey)
+        # return false unless valid_multiple_choice_settings?(survey)
+        surveys[survey["name"]] = survey
+      end
+
+      surveys
+    end
+
   end
 end
