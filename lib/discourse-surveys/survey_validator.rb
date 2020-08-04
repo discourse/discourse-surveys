39b33824876d047db9f6c5855e12b4a626689dfb
Arpit Jalan <arpit@techapj.com>
2020-08-04 20:47:37 +0530

    WIP: add validations for survey fields and field options.

--- lib/discourse-surveys/survey_validator.rb
+++ lib/discourse-surveys/survey_validator.rb
@@ -14,18 +14,76 @@ module DiscourseSurvey
 
       DiscourseSurvey::Helper::extract(@post.raw, @post.topic_id, @post.user_id).each do |survey|
         # return false unless valid_arguments?(survey)
-        # return false unless valid_numbers?(survey)
-        # return false unless unique_survey_name?(surveys, survey)
         # return false unless unique_options?(survey)
         # return false unless any_blank_options?(survey)
         # return false unless at_least_one_option?(survey)
-        # return false unless valid_number_of_options?(survey)
-        # return false unless valid_multiple_choice_settings?(survey)
 
         surveys[survey["name"]] = survey
       end
 
       surveys
+    end
+
+    private
+
+    def valid_arguments?(survey)
+      valid = true
+
+      survey["fields"].each do |field|
+        if field["response_type"].present? && !::SurveyField.response_type.has_key?(field["response_type"])
+          @post.errors.add(:base, I18n.t("survey.invalid_argument", argument: "response_type", value: field["response_type"]))
+          valid = false
+        end
+
+        unless field["question"].present?
+          @post.errors.add(:base, I18n.t("survey.blank_question"))
+          valid = false
+        end
+      end
+
+      valid
+    end
+
+    def unique_options?(survey)
+      if survey["options"].map { |o| o["id"] }.uniq.size != survey["options"].size
+        if survey["name"] == ::DiscourseSurvey::DEFAULT_SURVEY_NAME
+          @post.errors.add(:base, I18n.t("survey.default_survey_must_have_different_options"))
+        else
+          @post.errors.add(:base, I18n.t("survey.named_survey_must_have_different_options", name: survey["name"]))
+        end
+
+        return false
+      end
+
+      true
+    end
+
+    def any_blank_options?(survey)
+      if survey["options"].any? { |o| o["html"].blank? }
+        if survey["name"] == ::DiscourseSurvey::DEFAULT_SURVEY_NAME
+          @post.errors.add(:base, I18n.t("survey.default_survey_must_not_have_any_empty_options"))
+        else
+          @post.errors.add(:base, I18n.t("survey.named_survey_must_not_have_any_empty_options", name: survey["name"]))
+        end
+
+        return false
+      end
+
+      true
+    end
+
+    def at_least_one_option?(survey)
+      if survey["options"].size < 1
+        if survey["name"] == ::DiscourseSurvey::DEFAULT_SURVEY_NAME
+          @post.errors.add(:base, I18n.t("survey.default_survey_must_have_at_least_1_option"))
+        else
+          @post.errors.add(:base, I18n.t("survey.named_survey_must_have_at_least_1_option", name: survey["name"]))
+        end
+
+        return false
+      end
+
+      true
     end
 
   end
