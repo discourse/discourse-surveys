discourse/discourse-surveys

commit 05e64e659ebf3b4d03855cba3f895ea3806821db

DEV: Refactor UI from widgets to glimmer components (#58)
Also fixes some missing icons, and introduces a basic system spec. HTML structure is almost completely unchanged.

assets/javascripts/components/surveys/fields/options.gjs
@@ -1,0 +1,52 @@
+import Component from "@glimmer/component";
+import { fn } from "@ember/helper";
+import { on } from "@ember/modifier";
+import { action } from "@ember/object";
+import { service } from "@ember/service";
+import { modifier } from "ember-modifier";
+import icon from "discourse/helpers/d-icon";
+import { bind } from "discourse/lib/decorators";
+import { applyLocalDates } from "discourse/lib/local-dates";
+
+export default class SurveyOptions extends Component {
+  @service siteSettings;
+
+  applyLocalDatesModifier = modifier((element) => {
+    applyLocalDates(
+      element.querySelectorAll(".discourse-local-date"),
+      this.siteSettings
+    );
+  });
+
+  @action
+  toggle(option) {
+    this.args.toggleOption({
+      fieldId: this.args.field.digest,
+      option,
+      isMultiple: this.args.field.is_multiple_choice,
+    });
+  }
+
+  @bind
+  iconFor(option) {
+    const isChosen = this.args.response[this.args.field.digest]?.includes(
+      option.digest
+    );
+
+    if (this.args.field.is_multiple_choice) {
+      return isChosen ? "far-square-check" : "far-square";
+    } else {
+      return isChosen ? "far-circle-check" : "far-circle";
+    }
+  }
+
+  <template>
+    {{! template-lint-disable no-invalid-interactive }}
+    {{#each @field.options as |option|}}
+      <li class="survey-field-option" {{on "click" (fn this.toggle option)}}>
+        {{icon (this.iconFor option)}}
+        <span {{this.applyLocalDatesModifier}}>{{option.html}}</span>
+      </li>
+    {{/each}}
+  </template>
+}

app/models/survey_response.rb
@@ -2,6 +2,7 @@
 
 class SurveyResponse < ActiveRecord::Base
   belongs_to :survey_field
+  belongs_to :survey_field_option
 end
 
 # == Schema Information

assets/javascripts/api-initializers/extend-for-survey.gjs
@@ -1,0 +1,44 @@
+import routeAction from "discourse/helpers/route-action";
+import { apiInitializer } from "discourse/lib/api";
+import Survey from "../components/surveys/survey";
+
+const SurveyShim = <template>
+  <Survey
+    @surveyName={{@data.surveyName}}
+    @post={{@data.post}}
+    @showLogin={{routeAction "showLogin"}}
+  />
+</template>;
+
+export default apiInitializer((api) => {
+  api.modifyClass("controller:topic", {
+    pluginId: "discourse-surveys",
+    subscribe() {
+      this._super(...arguments);
+      this.messageBus.subscribe("/surveys/" + this.get("model.id"), (msg) => {
+        const post = this.get("model.postStream").findLoadedPost(msg.post_id);
+        if (post) {
+          post.set("surveys", msg.surveys);
+        }
+      });
+    },
+    unsubscribe() {
+      this.messageBus.unsubscribe("/surveys/*");
+      this._super(...arguments);
+    },
+  });
+
+  api.decorateCookedElement((element, helper) => {
+    const post = helper.getModel();
+
+    if (!post) {
+      return;
+    }
+
+    element.querySelectorAll(".survey").forEach((surveyElement) => {
+      const surveyName = surveyElement.dataset.surveyName;
+      surveyElement.innerHTML = "";
+      helper.renderGlimmer(surveyElement, SurveyShim, { surveyName, post });
+    });
+  });
+});

assets/javascripts/components/surveys/field.gjs
@@ -1,0 +1,65 @@
+import SurveyDropdown from "./fields/dropdown";
+import SurveyNumber from "./fields/number";
+import SurveyOptions from "./fields/options";
+import SurveyStar from "./fields/star";
+import SurveyTextarea from "./fields/textarea";
+import SurveyThumbs from "./fields/thumbs";
+
+const FIELDS = {
+  0: {
+    name: "radio",
+    component: SurveyOptions,
+  },
+  1: {
+    name: "checkbox",
+    component: SurveyOptions,
+  },
+  2: {
+    name: "number",
+    component: SurveyNumber,
+  },
+  3: {
+    name: "textarea",
+    component: SurveyTextarea,
+  },
+  4: {
+    name: "star",
+    component: SurveyStar,
+  },
+  5: {
+    name: "thumbs",
+    component: SurveyThumbs,
+  },
+  6: {
+    name: "dropdown",
+    component: SurveyDropdown,
+  },
+  unknown: {
+    name: "unknown",
+    component: <template>Unknown field type</template>,
+  },
+};
+
+function configFor(field) {
+  return FIELDS[field.response_type] || FIELDS["unknown"];
+}
+
+<template>
+  <div class="survey-field" data-survey-field-id={{@field.digest}}>
+    <span class="field-question">
+      <span>{{@field.question}}</span>
+    </span>
+
+    {{#let (configFor @field) as |fieldConfig|}}
+      <div class="field-{{fieldConfig.name}}">
+        <fieldConfig.component
+          @postId={{@postId}}
+          @field={{@field}}
+          @toggleValue={{@toggleValue}}
+          @toggleOption={{@toggleOption}}
+          @response={{@response}}
+        />
+      </div>
+    {{/let}}
+  </div>
+</template>

assets/javascripts/components/surveys/fields/dropdown.gjs
@@ -1,0 +1,25 @@
+import Component from "@glimmer/component";
+import { on } from "@ember/modifier";
+import { action } from "@ember/object";
+import { htmlSafe } from "@ember/template";
+
+export default class SurveyDropdown extends Component {
+  @action
+  changed(event) {
+    this.args.toggleValue({
+      fieldId: this.args.field.digest,
+      value: event.target.value,
+    });
+  }
+
+  <template>
+    <select class="survey-field-dropdown" {{on "change" this.changed}}>
+      <option label=" "></option>
+      {{#each @field.options as |optionInfo|}}
+        <option value={{optionInfo.digest}}>{{htmlSafe
+            optionInfo.html
+          }}</option>
+      {{/each}}
+    </select>
+  </template>
+}

assets/javascripts/components/surveys/fields/number.gjs
@@ -1,0 +1,35 @@
+import Component from "@glimmer/component";
+import { fn } from "@ember/helper";
+import { on } from "@ember/modifier";
+import { action } from "@ember/object";
+import icon from "discourse/helpers/d-icon";
+import { bind } from "discourse/lib/decorators";
+
+const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export default class SurveyNumber extends Component {
+  @action
+  toggle(value) {
+    this.args.toggleValue({
+      fieldId: this.args.field.digest,
+      value,
+    });
+  }
+
+  @bind
+  iconFor(value) {
+    const isChosen = this.args.response[this.args.field.digest] === value;
+
+    return isChosen ? "far-circle-check" : "far-circle";
+  }
+
+  <template>
+    {{! template-lint-disable no-invalid-interactive }}
+    {{#each NUMBERS as |value|}}
+      <li class="survey-field-number" {{on "click" (fn this.toggle value)}}>
+        {{icon (this.iconFor value)}}
+        <span {{this.applyLocalDatesModifier}}>{{value}}</span>
+      </li>
+    {{/each}}
+  </template>
+}

assets/javascripts/components/surveys/fields/star.gjs
@@ -1,0 +1,50 @@
+import Component from "@glimmer/component";
+import { on } from "@ember/modifier";
+import { action } from "@ember/object";
+import { gt } from "truth-helpers";
+import icon from "discourse/helpers/d-icon";
+
+const RATINGS = [0, 1, 2, 3, 4, 5];
+
+export default class SurveyStar extends Component {
+  @action
+  changed(event) {
+    const value = event.currentTarget.querySelector(`input:checked`)?.value;
+    this.args.toggleValue({
+      value,
+      fieldId: this.args.field.digest,
+    });
+  }
+
+  <template>
+    <div class="survey-field-star" {{on "input" this.changed}}>
+      {{#each RATINGS as |value|}}
+        {{#if (gt value 0)}}
+          <label
+            class="star-rating-label"
+            for="star-rating-{{@postId}}-{{value}}"
+          >
+            {{icon "star"}}
+          </label>
+          <input
+            id="star-rating-{{@postId}}-{{value}}"
+            name="star-rating-{{@postId}}"
+            class="star-rating-input"
+            value={{value}}
+            type="radio"
+          />
+        {{else}}
+          <input
+            id="star-rating-{{@postId}}-0"
+            name="star-rating-{{@postId}}"
+            disabled
+            checked
+            class="star-rating-input"
+            value="0"
+            type="radio"
+          />
+        {{/if}}
+      {{/each}}
+    </div>
+  </template>
+}

assets/javascripts/components/surveys/fields/textarea.gjs
@@ -1,0 +1,20 @@
+import Component from "@glimmer/component";
+import { on } from "@ember/modifier";
+import { action } from "@ember/object";
+
+export default class SurveyTextarea extends Component {
+  @action
+  changed(e) {
+    const value = e.target.value.replace(/[\u200B-\u200D\uFEFF]/, "");
+    this.args.toggleValue({
+      value,
+      fieldId: this.args.field.digest,
+    });
+  }
+
+  <template>
+    <span>
+      <textarea {{on "input" this.changed}}></textarea>
+    </span>
+  </template>
+}

commit 8d8d1148ff875395eec20d5e4fe38fce5de6c3a2

FIX: Emoji rendering in radio options
Regressed in 05e64e659ebf3b4d03855cba3f895ea3806821db

assets/javascripts/components/surveys/fields/options.gjs
@@ -3,6 +3,7 @@ import { fn } from "@ember/helper";
 import { on } from "@ember/modifier";
 import { action } from "@ember/object";
 import { service } from "@ember/service";
+import { htmlSafe } from "@ember/template";
 import { modifier } from "ember-modifier";
 import icon from "discourse/helpers/d-icon";
 import { bind } from "discourse/lib/decorators";
@@ -45,7 +46,7 @@ export default class SurveyOptions extends Component {
     {{#each @field.options as |option|}}
       <li class="survey-field-option" {{on "click" (fn this.toggle option)}}>
         {{icon (this.iconFor option)}}
-        <span {{this.applyLocalDatesModifier}}>{{option.html}}</span>
+        <span {{this.applyLocalDatesModifier}}>{{htmlSafe option.html}}</span>
       </li>
     {{/each}}
   </template>

spec/lib/pretty_text_spec.rb
@@ -8,8 +8,8 @@ describe PrettyText do
       md = <<~MD
         [survey name="awesome-survey"]
         [radio question="Choose any one option:"]
-        - cat
-        - dog
+        - 🐈 cat
+        - 🐕 dog
         [/radio]
 
         [checkbox question="Choose multiple options:"]
@@ -44,8 +44,8 @@ describe PrettyText do
         <div class="survey" data-survey-name="awesome-survey">
         <div class="survey-radio" data-survey-type="radio" data-survey-field-id="45de436cbdb15bbd9a11cb0301733189" data-survey-question="Choose any one option:">
         <ul>
-        <li data-survey-option-id="8b4392fd5150b6ee8422ae2be97e3ac1">cat</li>
-        <li data-survey-option-id="1f85df8c4bbd305cf4364641922c06c7">dog</li>
+        <li data-survey-option-id="0081803fadf8c67971a671882640b3be"><img src="/images/emoji/twitter/cat.png?v=14" title=":cat:" class="emoji" alt=":cat:" loading="lazy" width="20" height="20"> cat</li>
+        <li data-survey-option-id="469b4f3d4617f34a816751fd918135d9"><img src="/images/emoji/twitter/dog.png?v=14" title=":dog:" class="emoji" alt=":dog:" loading="lazy" width="20" height="20"> dog</li>
         </ul>
         </div>
         <div class="survey-checkbox" data-survey-type="checkbox" data-survey-field-id="481e5197af6b16a4d1e12e12a3addc6e" data-survey-question="Choose multiple options:">